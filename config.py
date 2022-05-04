1a0e745533941ad0d82176f4018872f13bf05a54
Levi Cailleret <levi.cailleret@raiffeisen.ch>
2022-05-04 10:31:15 +0000

    cleanup

--- config.py
+++ config.py
@@ -3,21 +3,21 @@ import os
 
 CONFIG_FILE = os.path.join(os.path.dirname(__file__), "config.json")
 
-
 def get_value(value):
-
     try:
         with open(CONFIG_FILE) as jsonfile:
-
             data = json.load(jsonfile)
 
         if value in data:
             return data[value]
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
-        import os
-        print(os.getcwd())
-        raise Exception(e)
 
-    raise Exception(f"Unknown field: {value}. Available fields are: {data.keys()}")
+    except Exception as e:
+            return ""
+
+#        import traceback
+#        traceback.print_exc()
+#        import os
+#        print(os.getcwd())
+#        raise Exception(e)
+#
+#    raise Exception(f"Unknown field: {value}. Available fields are: {data.keys()}")
