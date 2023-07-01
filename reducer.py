27775cd75e5666b0cdc7c803df5ba3f2382ef8a6
Dobin <dobin@broken.ch>
2023-07-01 20:54:10 +0200

    feature: handle very-long scans better (increasing minMatchSize with time)

--- reducer.py
+++ reducer.py
@@ -10,32 +10,48 @@ from plugins.model import BaseFile
 
 from utils import *
 
-SIG_SIZE = 8 # minimum size of a match
-PRINT_DELAY_SECONDS = 1
+PRINT_DELAY_SECONDS = 2
 
 
 class Reducer():
     """Reducer will scan data in file with scanner, and return List of matches"""
 
-    def __init__(self, file: BaseFile, scanner: Scanner):
+    def __init__(self, file: BaseFile, scanner: Scanner, extensiveScan=False):
         self.file: BaseFile = file
-        self.scanner = scanner
+        self.scanner: Scanner = scanner
+        self.extensiveScan: bool = extensiveScan
 
+        # re-init for every scan
         self.lastPrintTime: int = 0
+        self.it = IntervalTree()
+        self.matchesAdded: int = 0
+
+        # will stay for subsequent scans (e.g. when scanning one section after another)
+        self.minMatchSize: int = 8
         self.chunks_tested: int = 0
         self.iterations: int = 0
         self.matchIdx: int = 0
 
 
+    def init(self):
+        self.it = IntervalTree()
+        self.lastPrintTime = 0
+
+
     def scan(self, offsetStart, offsetEnd) -> List[Match]:
         """Scan self.file.Data() from offsetStart to offsetEnd, return matches"""
-        it = IntervalTree()
+        self.init()
         data = self.file.Data()  # get the data of the file to work on
-        self._scanDataPart(data, offsetStart, offsetEnd, it)
-        it.merge_overlaps(strict=False)
-        it = sorted(it)
 
-        matches = convertMatchesIt(it, self.iterations, self.matchIdx)
+        timeStart = time.time()
+        self._scanDataPart(data, offsetStart, offsetEnd)
+        timeEnd = time.time()
+
+        scanTime = round(timeEnd - timeStart)
+        #logging.info("Scan time: {}".format(scanTime))
+        logging.info("SCAN RESULT: Time:{} Chunks:{} MatchesAdd:{} -> MatchesFinal:{}".format(
+            scanTime, self.chunks_tested, self.matchesAdded, len(self.it)))
+        matches = convertMatchesIt(self.it, self.iterations, self.matchIdx)
         self.matchIdx += len(matches)
         self.iterations += 1
 
@@ -48,22 +64,44 @@ class Reducer():
         return self.scanner.scannerDetectsBytes(newFileData.getBytes(), self.file.filename)
 
 
+    def _addMatch(self, sectionStart: int, sectionEnd: int):
+        self.it.add ( Interval(sectionStart, sectionEnd) )
+        self.matchesAdded += 1
+
+        # Always merge, so we have accurate information about the amount of real matches
+        self.it.merge_overlaps(strict=False)
+
+
+
     # recursive
-    def _scanDataPart(self, data: Data, sectionStart, sectionEnd, it):
+    def _scanDataPart(self, data: Data, sectionStart: int, sectionEnd: int):
         size = sectionEnd - sectionStart
         chunkSize = int(size // 2)
+        self.chunks_tested += 1
         self._printStatus()
-        
+
+        # adjust for long scan times
+        # double every x matches, starting from y
+        # This should make the algo not go so deep, and cover multiple matches,
+        # which means less scans. but also broader matches.
+        if not self.extensiveScan:
+            if self.chunks_tested >= 220 and self.chunks_tested % 110 == 0:
+                self.minMatchSize *= 2
+                logging.warn("Doubling minMatchSize to {}".format(self.minMatchSize))
+
         #logging.info(f"Testing: {sectionStart}-{sectionEnd} with size {sectionEnd-sectionStart} (chunkSize {chunkSize} bytes)")
         #logging.info(f"Testing Top: {sectionStart}-{sectionStart+chunkSize}")
         #logging.info(f"Testing Bot: {sectionStart+chunkSize}-{sectionStart+chunkSize+chunkSize}")
 
-        if chunkSize < 2:
-            # dangling bytes
+        # dangling bytes
+        # note that these have been detected, thats why they are being scanned.
+        # so we can just add them
+        if chunkSize <= 2:
             dataBytes = data.getBytesRange(sectionStart, sectionEnd)
             logging.info(f"Result: {sectionStart}-{sectionEnd} ({sectionEnd-sectionStart} bytes)" 
                             + "\n" + hexdmp(dataBytes, offset=sectionStart))
-            it.add ( Interval(sectionStart, sectionEnd) )
+            self._addMatch(sectionStart, sectionEnd)
+            
             return
 
         dataChunkTopNull = deepcopy(data)
@@ -80,42 +118,41 @@ class Reducer():
             # Both halves are detected
             # Continue scanning both halves independantly, but with each other halve
             # zeroed out (instead of the complete file)
-            self._scanDataPart(dataChunkBotNull, sectionStart, sectionStart+chunkSize, it)
-            self._scanDataPart(dataChunkTopNull, sectionStart+chunkSize, sectionEnd, it)
+            self._scanDataPart(dataChunkBotNull, sectionStart, sectionStart+chunkSize)
+            self._scanDataPart(dataChunkTopNull, sectionStart+chunkSize, sectionEnd)
 
         elif not detectTopNull and not detectBotNull:
             #logging.info("--> Both UNdetected")
             # both parts arent detected anymore
 
-            if chunkSize <= SIG_SIZE:
+            if chunkSize <= self.minMatchSize:
                 # Small enough, no more detections.
                 # The "previous" section is our match
                 dataBytes = data.getBytesRange(sectionStart, sectionStart+size)
                 logging.info(f"Result: {sectionStart}-{sectionEnd} ({sectionEnd-sectionStart} bytes)" 
                              + "\n" + hexdmp(dataBytes, offset=sectionStart))
-                it.add ( Interval(sectionStart, sectionStart+size) )
+                self._addMatch(sectionStart, sectionStart+size)
             else: 
                 # make it smaller still. 
                 # Take complete data (not nulled)
-                self._scanDataPart(data, sectionStart, sectionStart+chunkSize, it)
-                self._scanDataPart(data, sectionStart+chunkSize, sectionEnd, it)
+                self._scanDataPart(data, sectionStart, sectionStart+chunkSize)
+                self._scanDataPart(data, sectionStart+chunkSize, sectionEnd)
 
         elif not detectTopNull:
             # Detection in the top half
             #logging.info("--> Do Top")
-            self._scanDataPart(data, sectionStart, sectionStart+chunkSize, it)
+            self._scanDataPart(data, sectionStart, sectionStart+chunkSize)
         elif not detectBotNull:
             # Detection in the bottom half
             #logging.info("--> Do Bot")
-            self._scanDataPart(data, sectionStart+chunkSize, sectionEnd, it)
+            self._scanDataPart(data, sectionStart+chunkSize, sectionEnd)
 
         return
 
 
     def _printStatus(self):
-        self.chunks_tested += 1
-
         currentTime = time.time()
         if currentTime > self.lastPrintTime + PRINT_DELAY_SECONDS:
             self.lastPrintTime = currentTime
-            logging.info("Reducing: {} chunks done".format(self.chunks_tested))
+            logging.info("Reducing: {} chunks done, found {} matches ({} added)".format(
+                self.chunks_tested, len(self.it), self.matchesAdded))
